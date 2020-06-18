497d41233f4d20cabbfafc61061b8deb2e75276b
navid <navidsoleymani@ymail.com>
2020-06-18 09:26:32 +0430

    init

---
+++ local_library/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
